2f5d44eef615348e22752fee7fdf2fa494657d26
Richard Lee <richlee81192@gmail.com>
2020-03-04 19:28:40 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,11 +3,11 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
+  def self.scrape_index_page
     index = Nokogiri::HTML(open(https://learn-co-curriculum.github.io/student-scraper-test-page/))
   end
 
-  def self.scrape_profile_page(profile_url)
+  def self.scrape_profile_page
     self.scrape_index_page.css(".student-card")
     
     binding 'pry'
