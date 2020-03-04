38d10a4333e9c42424cc8fa9e308fab3b86d269b
Richard Lee <richlee81192@gmail.com>
2020-03-04 20:02:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,7 +9,9 @@ class Student
   end
 
   def self.create_from_collection(students_array)
+    self.all.each do |student|
     students = self.new
+    students.
   end
 
   def add_student_attributes(attributes_hash)
